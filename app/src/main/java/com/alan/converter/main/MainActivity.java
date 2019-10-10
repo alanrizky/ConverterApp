e6ff121c797cd8916b880fc40b272d477ab56bc3
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-10-10 21:32:27 +0700

    Add feature = Speed Converter

--- app/src/main/java/com/alan/converter/main/MainActivity.java
+++ app/src/main/java/com/alan/converter/main/MainActivity.java
@@ -9,11 +9,16 @@ import android.view.View;
 import com.alan.converter.R;
 import com.alan.converter.fragments.DigitalStorageFragment;
 import com.alan.converter.fragments.LengthConverterFragment;
+import com.alan.converter.fragments.SpeedFragment;
 
-public class MainActivity extends AppCompatActivity implements LengthConverterFragment.OnFragmentInteractionListener, DigitalStorageFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements
+             LengthConverterFragment.OnFragmentInteractionListener,
+             DigitalStorageFragment.OnFragmentInteractionListener,
+             SpeedFragment.OnFragmentInteractionListener{
 
     private LengthConverterFragment lengthConverterFragment;
     private DigitalStorageFragment digitalStorageFragment;
+    private SpeedFragment speedFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements LengthConverterFr
         setContentView(R.layout.activity_main);
         lengthConverterFragment = new LengthConverterFragment();
         digitalStorageFragment = new DigitalStorageFragment();
+        speedFragment = new SpeedFragment();
 
     }
 
@@ -32,11 +38,19 @@ public class MainActivity extends AppCompatActivity implements LengthConverterFr
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, digitalStorageFragment).commit();
     }
 
+    public void speedHandler(View view) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, speedFragment).commit();
+    }
+
     public void onConvertLengthButtonClicked(View view) {
         lengthConverterFragment.setResult();
     }
 
     public void onConvertDigitalStorageButtonClicked(View view) {
         digitalStorageFragment.setResult();
+    }
+
+    public void onConvertSpeedButtonClicked(View view) {
+        speedFragment.setResult();
     }
 }

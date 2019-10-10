2e65960ac3b8229d9c1e6c1be8ed6f34dbb9638c
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-10-10 20:37:42 +0700

    Add feature = Digital Storage converter

--- app/src/main/java/com/alan/converter/main/MainActivity.java
+++ app/src/main/java/com/alan/converter/main/MainActivity.java
@@ -7,17 +7,20 @@ import android.view.View;
 
 
 import com.alan.converter.R;
+import com.alan.converter.fragments.DigitalStorageFragment;
 import com.alan.converter.fragments.LengthConverterFragment;
 
-public class MainActivity extends AppCompatActivity implements LengthConverterFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements LengthConverterFragment.OnFragmentInteractionListener, DigitalStorageFragment.OnFragmentInteractionListener{
 
     private LengthConverterFragment lengthConverterFragment;
+    private DigitalStorageFragment digitalStorageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lengthConverterFragment = new LengthConverterFragment();
+        digitalStorageFragment = new DigitalStorageFragment();
 
     }
 
@@ -25,7 +28,15 @@ public class MainActivity extends AppCompatActivity implements LengthConverterFr
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, lengthConverterFragment).commit();
     }
 
-    public void onConvertButtonClicked(View view) {
+    public void digitalStorageHandler(View view) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, digitalStorageFragment).commit();
+    }
+
+    public void onConvertLengthButtonClicked(View view) {
         lengthConverterFragment.setResult();
+    }
+
+    public void onConvertDigitalStorageButtonClicked(View view) {
+        digitalStorageFragment.setResult();
     }
 }

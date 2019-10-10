f53bd953191f2795c181dabc59cc15ff4aef3659
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-10-10 23:21:55 +0700

    Add feature = Speed Converter

--- app/src/main/java/com/alan/converter/converter/SpeedConverter.java
+++ app/src/main/java/com/alan/converter/converter/SpeedConverter.java
@@ -6,7 +6,7 @@ public class SpeedConverter {
         mph,
         knot,
         mmh,
-        fmh;
+        fph;
 
         public static Speed fromString(String text) {
             if (text != null) {
@@ -26,38 +26,47 @@ public class SpeedConverter {
         double k = 1;
         switch (from) {
             case kmh:
-                if (to == Speed.mph) {
+                if (to == Speed.kmh) {
+                    k = 1;
+                } else if (to == Speed.mph) {
                     k = 0.621371;
                 } else if (to == Speed.knot) {
                     k = 0.539957;
                 } else if (to == Speed.mmh) {
                     k = 0.277778;
-                } else if (to == Speed.fmh) {
+                } else if (to == Speed.fph) {
                     k = 0.911344;
                 }
                 break;
+
             case mph:
                 if (to == Speed.kmh) {
                     k = 1.60934;
+                } else if (to == Speed.mph) {
+                    k = 1;
                 } else if (to == Speed.knot) {
                     k = 0.868976;
                 } else if (to == Speed.mmh) {
                     k = 0.44704;
-                } else if (to == Speed.fmh) {
+                } else if (to == Speed.fph) {
                     k = 1.46667;
-                    break;
                 }
+                break;
+
             case knot:
                 if (to == Speed.kmh) {
                     k = 1.852;
                 } else if (to == Speed.mph) {
                     k = 1.15078;
+                } else if (to == Speed.knot) {
+                    k = 1;
                 } else if (to == Speed.mmh) {
                     k = 0.514444;
-                } else if (to == Speed.fmh) {
+                } else if (to == Speed.fph) {
                     k = 1.68781;
                 }
                 break;
+
             case mmh:
                 if (to == Speed.kmh) {
                     k = 3.6;
@@ -65,11 +74,14 @@ public class SpeedConverter {
                     k = 2.23694;
                 } else if (to == Speed.knot) {
                     k = 1.94384;
-                } else if (to == Speed.fmh) {
+                } else if (to == Speed.mmh) {
+                    k = 1;
+                } else if (to == Speed.fph) {
                     k = 3.28084;
                 }
                 break;
-            case fmh:
+
+            case fph:
                 if (to == Speed.kmh) {
                     k = 1.09728;
                 } else if (to == Speed.mph) {
@@ -78,11 +90,14 @@ public class SpeedConverter {
                     k = 0.592484;
                 } else if (to == Speed.mmh) {
                     k = 0.3048;
+                } else if (to == Speed.fph) {
+                    k = 1;
                 }
                 break;
         }
         kali = k;
     }
+
     public double converted(double input) {
         return input * kali;
     }
